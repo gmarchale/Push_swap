16a88620bdd308c0775d7fd1f94ca25dff4de1a6
Gaetan <gaetan.marchale@hotmail.com>
2023-01-30 17:42:37 +0100

    reverse rotate functions

---
+++ src/reverse_rotate.c
@@ -0,0 +1,55 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   reverse_rotate.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gmarchal <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/01/30 16:46:52 by gmarchal          #+#    #+#             */
+/*   Updated: 2023/01/30 17:41:02 by gmarchal         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "push_swap.h"
+
+void	reverse_rotate(t_list **stack);
+
+void	rra(t_list **stack)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+		return ;
+	ft_printf("rra\n");
+	reverse_rotate(stack);
+}
+
+void	rrb(t_list **stack)
+{
+	if (*stack == NULL || (*stack)->next == NULL)
+		return ;
+	ft_printf("rrb\n");
+	reverse_rotate(stack);
+}
+
+void	rrr(t_list **stack_a, t_list **stack_b)
+{
+	if (*stack_a == NULL || (*stack_a)->next == NULL)
+		return ;
+	if (*stack_b == NULL || (*stack_b)->next == NULL)
+		return ;
+	ft_printf("rrr\n");
+	reverse_rotate(stack_a);
+	reverse_rotate(stack_b);
+}
+
+void	reverse_rotate(t_list **stack)
+{
+	t_list	*last_node;
+	t_list	*tmp;
+
+	tmp = *stack;
+	while (tmp->next->next != NULL)
+		tmp = tmp->next;
+	last_node = tmp->next;
+	tmp->next = NULL;
+	ft_lstadd_front(stack, last_node);
+}
