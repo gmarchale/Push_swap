352f2d0f1918baba322b36037645b12010cdd5fd
Gaetan <gaetan.marchale@hotmail.com>
2023-01-30 12:55:39 +0100

    Update Libft

--- Libft/ft_strlcpy.c
+++ Libft/ft_strlcpy.c
@@ -1,13 +1,32 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   swap.c                                             :+:      :+:    :+:   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: gmarchal <marvin@42.fr>                    +#+  +:+       +#+        */
+/*   By: gmarchal <gmarchal@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/01/27 17:16:22 by gmarchal          #+#    #+#             */
-/*   Updated: 2023/01/27 17:17:22 by gmarchal         ###   ########.fr       */
+/*   Created: 2022/10/04 14:46:44 by gmarchal          #+#    #+#             */
+/*   Updated: 2022/10/13 13:24:44 by gmarchal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include <unistd.h>
 
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	if (size > 0)
+	{
+		while (src[i] && i < (size - 1))
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = 0;
+	}
+	while (src[i])
+		i++;
+	return (i);
+}
