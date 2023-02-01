4f6ebf3329d9b9e33264753a6f4fff7f7852d44f
Gaetan <gaetan.marchale@hotmail.com>
2023-02-01 20:32:18 +0100

    Petite main qui print ma stack avec les arguments dedans

--- src/main.c
+++ src/main.c
@@ -6,21 +6,21 @@
 /*   By: gmarchal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 17:09:58 by gmarchal          #+#    #+#             */
-/*   Updated: 2023/01/27 17:15:28 by gmarchal         ###   ########.fr       */
+/*   Updated: 2023/02/01 20:31:54 by gmarchal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-int main(argc, char *argv)
+#include "push_swap.h"
+
+int	main(int argc, char **argv)
 {
 	t_list	*stack_a;
 	t_list	*stack_b;
-	int		is_string;
 
 	if (argc == 1)
 		return (0);
-	is_string = 0;
-	if (argc == 2) // pour check comment est l'input car si string tout est deplace d'un cran
-		is_string == 1;
-	//encore des trucs a faire
+	stack_a = init_stack(argc, argv);
+	stack_b = NULL;
+	print_stack(stack_a);
 	return (0);
 }
