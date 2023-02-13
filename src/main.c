f9920c0b0e090e83131a35166fd7bebe852f5a94
Gaetan <gaetan.marchale@hotmail.com>
2023-02-13 18:35:21 +0100

    test function mod_values

--- src/main.c
+++ src/main.c
@@ -6,12 +6,14 @@
 /*   By: gmarchal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 17:09:58 by gmarchal          #+#    #+#             */
-/*   Updated: 2023/02/09 14:20:49 by gmarchal         ###   ########.fr       */
+/*   Updated: 2023/02/13 18:29:42 by gmarchal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 #include <stdio.h> // delete
+
+void	mod_stack(t_list **stack, int *array_mod_values);
 
 int	len_array(int *array)
 {
@@ -23,43 +25,53 @@ int	len_array(int *array)
 	return (i);
 }
 
-int	*sort_array(int *array)
+void	mod_values(int *array, int len_stack, t_list **stack)
 {
 	int	i;
 	int	j;
-	int	n;
-	int	len;
-	int	*new;
+	int	count;
+	int	*cp_array;
+	int q = 0;
 
 	i = 0;
-	len = len_array(array);
-	new = malloc(sizeof(int) * len); // a free
-	if (!new)
-		return (0);
-	ft_bzero(new, len);
-	while (array[i])
+	cp_array = init_array(len_stack, stack);
+	while(array[q])
 	{
-		n = 0;
+		printf("%d\n", cp_array[q]);
+		printf("q = %d\n", q);
+		q++;
+	}
+	printf("-------------------------");
+	while (i < len_stack)
+	{
 		j = 0;
-		while(array[j])
+		count = 0;
+		while (j < len_stack)
 		{
-			if (array[j] < array[i])
-				n++;
+			if (cp_array[j] < cp_array[i])
+				count++;
 			j++;
 		}
-		if (n == 1)
-			printf("n = 1\n");
-		new[n] = array[i];
-		printf("new[%d] = %d\n", n, new[n]);
+		array[i] = count;
 		i++;
 	}
-	i = 0;
-	while (i < 10)
+	free (cp_array);
+	mod_stack(stack, array);
+}
+
+void	mod_stack(t_list **stack, int *array_mod_values)
+{
+	t_list	*tmp;
+	int		a;
+
+	a = 0;
+	tmp = *stack;
+	while (tmp)
 	{
-		printf("%d\n", new[i]);
-		i++;
+		tmp->content = array_mod_values[a];
+		a++;
+		tmp = tmp->next;
 	}
-	return (new);
 }
 
 int	main(int argc, char **argv)
@@ -67,24 +79,12 @@ int	main(int argc, char **argv)
 	t_list	*stack_a;
 	t_list	*stack_b;
 	int	*array;
-	int	i;
-	int	*new;
 
 	if (argc == 1)
 		return (0);
 	stack_a = init_stack(argc, argv);
 	stack_b = NULL;
-	//print_stack(stack_a);
-	//ft_printf("Len de la stack: %d\n", len_stack(&stack_a));
 	array = init_array(len_stack(&stack_a), &stack_a);
-	i = 0;
-	new = sort_array(array);
-	/*
-	while (new[i])
-	{
-		printf("%d\n", new[i]);
-		i++;
-	}
-	*/
+	mod_values(array, len_stack(&stack_a), &stack_a); 
 	return (0);
 }
