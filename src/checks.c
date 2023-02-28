f105563206cd8f2a72d025d581e55d071714496c
Gaetan <gaetan.marchale@hotmail.com>
2023-02-28 18:50:11 +0100

    start checks.c

--- src/checks.c
+++ src/checks.c
@@ -6,7 +6,7 @@
 /*   By: gmarchal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/22 16:22:05 by gmarchal          #+#    #+#             */
-/*   Updated: 2023/02/27 18:42:39 by gmarchal         ###   ########.fr       */
+/*   Updated: 2023/02/28 18:49:37 by gmarchal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -37,4 +37,20 @@ int	check_valid_number(int argc, char **argv)
 	int	i;
 
 	i = 0;
+}
+
+
+int	check_input(int argc, char **argv)
+{
+	int	i;
+
+	if (argc == 1)
+		error();
+	if (check_duplicates(argc, argv) == 1)
+		error();
+	i = 1;
+	while (argv[i])
+	{
+
+	}
 }
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: gmarchal <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/27 17:09:58 by gmarchal          #+#    #+#             */
-/*   Updated: 2023/02/27 18:25:27 by gmarchal         ###   ########.fr       */
+/*   Updated: 2023/02/28 15:12:26 by gmarchal         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,8 @@ int	main(int argc, char *argv[])
 		min_sort(&stack_a, &stack_b);
 	else
 		radix_sort(&stack_a, &stack_b);
-	//printf("delete print avant de tests\n");
-	//print_stack(stack_a); //delete
+	printf("delete print avant de tests\n");
+	print_stack(stack_a); //delete
 	ft_lstclear(&stack_a);
 	return (0);
 }
